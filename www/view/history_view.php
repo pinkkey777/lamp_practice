1a36ce2fe7fb2cdce448f90fb8e5163d64ea48a3
suzuki <pinkkey777@gmail.com>
2021-01-07 17:09:07 +0900

    history_viewの作成

---
+++ www/view/history_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <?php include VIEW_PATH . 'templates/head.php'; ?>
+  <title>購入履歴</title>
+  <link rel="stylesheet" href="<?php print(h(STYLESHEET_PATH . 'admin.css')); ?>">
+</head>
+<body>
+  <table class="table table-bordered text-center">
+        <thead class="thead-light">
+          <tr>
+            <th>注文番号</th>
+            <th>購入日時</th>
+            <th>合計金額</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+  </table>
+</body>
+</html>
